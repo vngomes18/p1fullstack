34c1cba9caa2c0d3111e04fa9f5287c05b25790e
vngomes18 <contatovinicius20@gmail.com>
2026-09-16 20:02:27 -0300

    feat(admin): exibir e filtrar tipo_acervo e categoria no Django Admin

--- acervo/admin.py
+++ acervo/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
 from .models import Livro
 
-admin.site.register(Livro)
+
+@admin.register(Livro)
+class LivroAdmin(admin.ModelAdmin):
+    list_display = ('titulo', 'autor', 'ano', 'tipo_acervo', 'categoria', 'disponivel')
+    list_filter = ('tipo_acervo', 'categoria', 'disponivel')
+    search_fields = ('titulo', 'autor')
