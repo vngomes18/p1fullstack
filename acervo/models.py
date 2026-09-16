05122f0d13e53639e2795d623ebff5a3dc7f5bf7
vngomes18 <contatovinicius20@gmail.com>
2026-09-16 20:01:23 -0300

    feat(models): acrescentar tipo de acervo (Digital ou Físico) ao model Livro

--- acervo/models.py
+++ acervo/models.py
@@ -2,10 +2,19 @@ from django.db import models
 
 
 class Livro(models.Model):
+    class TipoAcervo(models.TextChoices):
+        DIGITAL = 'DIGITAL', 'Digital'
+        FISICO = 'FISICO', 'Físico'
+
     titulo = models.CharField(max_length=200)
     autor = models.CharField(max_length=100)
     ano = models.IntegerField()
     disponivel = models.BooleanField(default=True)
+    tipo_acervo = models.CharField(
+        max_length=10,
+        choices=TipoAcervo.choices,
+        default=TipoAcervo.FISICO,
+    )
 
     def __str__(self):
         return self.titulo
